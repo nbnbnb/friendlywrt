2dd74b9f5999fba0c61341fbc4ac2470f45d211d
nbnbnb <nbnbnb.zhang@gmail.com>
2023-05-24 00:43:48 +0800

    Update qca_ath10k.mk

--- package/firmware/linux-firmware/qca_ath10k.mk
+++ package/firmware/linux-firmware/qca_ath10k.mk
@@ -1,11 +1,11 @@
-QCA99X0_BOARD_REV:=ddcec9efd245da9365c474f513a855a55f3ac7fe
+QCA99X0_BOARD_REV:=e404444dfc0baf7d0fcde21ab8ec333608c9960c
 QCA99X0_BOARD_FILE:=board-2.bin.$(QCA99X0_BOARD_REV)
 
 define Download/qca99x0-board
-  URL:=https://source.codeaurora.org/quic/qsdk/oss/firmware/ath10k-firmware/plain/ath10k/QCA99X0/hw2.0
-  URL_FILE:=board-2.bin?id=$(QCA99X0_BOARD_REV)
+  URL:=https://github.com/kvalo/ath10k-firmware/raw/master/QCA99X0/hw2.0/
+  URL_FILE:=board-2.bin
   FILE:=$(QCA99X0_BOARD_FILE)
-  HASH:=03711ac21e60ef59d3815e235eb721c0c22851b5410299411085aa6f2af45401
+  HASH:=f91975dca2435fa6f8570146e6b255c2a70b9ffbdf5ef16a29d67bec7374c11a
 endef
 $(eval $(call Download,qca99x0-board))
 
